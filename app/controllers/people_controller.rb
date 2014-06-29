adb534b71ee9bc39a04c88d99f838cfc143c8c2c
Lucas Roman <lucasroman@gmail.com>
2014-06-29 18:22:01 -0300

    Agregadas las acciones create y show para Person.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -8,4 +8,22 @@ class PeopleController < ApplicationController
   	@person = Person.new
   end
 
+  def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to @person
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+  
+  private
+    def person_params
+      params.require(:person).permit(:name, :dni)
+    end
 end
