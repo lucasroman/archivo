165134d9448c7c87f166338f16e7f1c2220b335f
Lucas Roman <lucasroman@gmail.com>
2014-06-30 17:38:51 -0300

    Agregadas acciones edit y update en Person.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -22,6 +22,20 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
   end
   
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update(person_params)
+      redirect_to @person
+    else
+      render 'edit'
+    end
+  end
+
   private
     def person_params
       params.require(:person).permit(:name, :dni)
