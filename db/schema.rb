88056b9d091a72d4b22e8f010fa67fc0c868052a
Lucas Roman <lucasroman@gmail.com>
2014-07-02 16:13:26 -0300

    Creada tabla file_records.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701214255) do
+ActiveRecord::Schema.define(version: 20140702190308) do
 
   create_table "file_records", force: true do |t|
     t.string   "title"
