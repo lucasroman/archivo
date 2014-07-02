487cb5b2b48c96f66a363b7d12581c6e37d1cd97
Lucas Roman <lucasroman@gmail.com>
2014-07-02 18:11:39 -0300

    Agregados los ids de file_record, person y office en tabla steps.

--- db/migrate/20140627135355_create_steps.rb
+++ db/migrate/20140627135355_create_steps.rb
@@ -2,6 +2,9 @@ class CreateSteps < ActiveRecord::Migration
   def change
     create_table :steps do |t|
 
+      t.integer :file_record_id
+      t.integer :person_id
+      t.integer :office_id
       t.timestamps
     end
   end
