9de3ba331432b75e938d9ebae49cdad8a0a0cef8
Lucas Roman <lucasroman@gmail.com>
2014-07-02 11:52:26 -0300

    Agregado parámetro con nombre de la tabla.

--- db/migrate/20140630195746_create_file_records.rb
+++ db/migrate/20140630195746_create_file_records.rb
@@ -1,7 +1,7 @@
 class CreateFileRecords < ActiveRecord::Migration
   def change
     create_table :file_records do |t|
-      add_column :title, :string 
+      add_column :file_records, :title, :string 
       t.timestamps
     end
   end
