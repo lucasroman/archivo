e61efb0714b62558eed220d80a545ded13a74c42
Lucas Roman <lucasroman@gmail.com>
2014-07-04 10:33:09 -0300

    Sin cambios importantes.

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,6 @@ class Person < ActiveRecord::Base
 	has_many :steps
 
 	validates :name, presence: true, length: { minimum: 3 }
-	validates :dni, presence: true, length: { minimum: 8}
+	validates :dni, presence: true, length: { minimum: 8 }
 	
 end
