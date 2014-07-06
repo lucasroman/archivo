7834fd908b96eaf31d076798cd55bcf35cc58a86
Lucas Roman <lucasroman@gmail.com>
2014-07-06 20:41:05 -0300

    Cambiado el new de Office por create así la crea y la guarda.

--- app/models/step.rb
+++ app/models/step.rb
@@ -4,13 +4,13 @@ class Step < ActiveRecord::Base
   	office = Office.where(name: 'Mesa de entrada').first!
 
     if office.name != 'Mesa de entrada'
-      office = Office.new(name: 'Mesa de entrada')
+      office = Office.create(name: 'Mesa de entrada')
     end
 
     step = Step.new
     office.steps << step
   end
-  
+
   belongs_to :person
   belongs_to :office
   belongs_to :file_record
