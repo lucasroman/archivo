3044825eb029817e378e86e71c5a3df4fb4c93e2
Lucas Roman <lucasroman@gmail.com>
2014-07-06 20:17:09 -0300

    Primera versión del método start para modelo step.

--- app/models/step.rb
+++ app/models/step.rb
@@ -4,4 +4,15 @@ class Step < ActiveRecord::Base
   belongs_to :office
   belongs_to :file_record
   
+  def start
+  	office = Office.where(name: 'Mesa de entrada').first!
+
+    if office.name != 'Mesa de entrada'
+      office = Office.new(name: 'Mesa de entrada')
+    end
+
+    step = Step.new
+    office.steps << step
+  end
+  
 end
