3ca13218e646247ca5cadd28157fbe613a1b1101
Lucas Roman <lucasroman@gmail.com>
2014-07-07 18:53:06 -0300

    Agregado método "archivate" al modelo de Step.

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,7 +3,11 @@ class Step < ActiveRecord::Base
   def self.start
     office = Office.find_or_create_by(name: 'Mesa de entrada')
     step = Step.new(office: office)
-    office.steps << step
+  end
+
+  def self.archivate
+  	office = Office.find_or_create_by(name: 'Archivo')
+    step = Step.new(office: office)
   end
 
   belongs_to :person
