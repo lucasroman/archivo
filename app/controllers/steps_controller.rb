83ba1754b2732f6dbe38a9c16b351a575608e186
Lucas Roman <lucasroman@gmail.com>
2014-07-07 18:52:29 -0300

    Agregado controlador de Steps con acción create.

---
+++ app/controllers/steps_controller.rb
@@ -0,0 +1,16 @@
+class StepsController < ApplicationController
+  
+  def create
+    @file_record = FileRecord.find(params[:file_record_id])
+    #binding.pry
+    @file_record.steps.create(file_record_params)
+
+    redirect_to file_record_path(params[:file_record_id])
+  end
+
+  private
+    def file_record_params
+      params.require(:step).permit(:office_id, :person_id)
+    end
+
+end
