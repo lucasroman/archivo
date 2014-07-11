c69e598c275e8cb2535af6609930012d3a31dad3
Lucas Roman <lucasroman@gmail.com>
2014-07-11 10:06:26 -0300

    Agregado método "archivate" a clase Office.

--- app/models/office.rb
+++ app/models/office.rb
@@ -3,5 +3,14 @@ class Office < ActiveRecord::Base
 	has_many :steps, dependent: :destroy
 
 	validates :name, presence: true, length: { minimum: 3 }
+
+	def self.start_office
+	    Office.find_or_create_by(name: 'Mesa de entrada')
+	end
+
+	def archivate
+    office = Office.find_or_create_by(name: 'Archivo')
+    step = Step.new(office: office)
+  end
 	
 end
